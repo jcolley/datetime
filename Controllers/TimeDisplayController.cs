6f8dcac533fd6e40381c29d138f7023967020add
John Colley <webjocky@gmail.com>
2017-08-04 14:53:59 -0700

    initial commit

---
+++ Controllers/TimeDisplayController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace timedisplay.Controllers
+{
+    public class timedisplayController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+    }
+}
